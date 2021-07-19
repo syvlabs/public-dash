d80570f64cee73d4f0daa547bb77b3e31813ba26
iman-san <iman@syvlabs.com>
2021-07-19 18:51:50 +0800

    text polish

--- pages/fmm.tsx
+++ pages/fmm.tsx
@@ -1,10 +1,10 @@
 import React, { FC } from 'react';
 import Head from 'next/head'
-import moment from 'moment'
+import moment, { Moment } from 'moment'
 
 const LINKS: {[key: number]: any} = {
     1: {
-        name: "Chef's Cuisines",
+        name: "Chef's",
         url: "https://food-market-manila.myshopify.com/apps/bundles/bundle/34811",
     },
     2: {
@@ -22,8 +22,8 @@ const LINKS: {[key: number]: any} = {
 }
 
 const FMM: FC = () => {
-  const week: number = moment().add(2, 'days').isoWeek()
-  const index: number = ((week + 1) % 4) + 1;
+  const orderDate: Moment = moment().add(2, 'days')
+  const index: number = ((orderDate.isoWeek() + 1) % 4) + 1;
   return (
     <div>
       <Head>
@@ -32,13 +32,13 @@ const FMM: FC = () => {
       <div className="w-full">
           <div className="text-2xl px-8 mt-8 text-gray-700 font-bold">Food Market Shortcuts</div>
           <div 
-            className="my-4 w-80 rounded-lg text-white bg-green-500 hover:bg-green-600 active:bg-green-800 p-4 mx-8 font-bold select-none cursor-pointer shadow" 
+            className="my-4 w-96 rounded-lg text-white bg-green-500 hover:bg-green-600 active:bg-green-800 p-4 mx-8 font-bold select-none cursor-pointer shadow" 
             onClick={() => {navigator.clipboard.writeText("Pls deliver food before 12nn, pls leave food at lobby")}}>
-              1. Copy Delivery Message
+              1. Click to Copy Delivery Message
           </div>
           <a href={LINKS[index].url} target="_blank" rel="noreferrer">
-            <div className="my-4 w-80 rounded-lg text-white bg-blue-400 hover:bg-blue-500 active:bg-blue-700 p-4 mx-8 font-bold select-none cursor-pointer shadow">
-                2. Go to Page ({LINKS[index].name})
+            <div className="my-4 w-96 rounded-lg text-white bg-blue-400 hover:bg-blue-500 active:bg-blue-700 p-4 mx-8 font-bold select-none cursor-pointer shadow">
+                2. Click to Open {LINKS[index].name} Cuisines (for {orderDate.format("MMM D")})
             </div>
           </a>
       </div>
