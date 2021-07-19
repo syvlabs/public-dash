8cafd657cb6bbbc98af82103b0adf3b804898fdd
iman-san <iman@syvlabs.com>
2021-07-19 18:37:55 +0800

    test

--- pages/fmm.tsx
+++ pages/fmm.tsx
@@ -1,8 +1,29 @@
-import React, { FC, useState, useEffect } from 'react';
+import React, { FC } from 'react';
 import Head from 'next/head'
+import moment from 'moment'
+
+const LINKS: {[key: number]: any} = {
+    1: {
+        name: "Chef's Cuisines",
+        url: "https://food-market-manila.myshopify.com/apps/bundles/bundle/34811",
+    },
+    2: {
+        name: "European",
+        url: "https://food-market-manila.myshopify.com/apps/bundles/bundle/34812",
+    },
+    3: {
+        name: "Asian",
+        url: "https://food-market-manila.myshopify.com/apps/bundles/bundle/34098",
+    },
+    4: {
+        name: "Filipino",
+        url: "https://food-market-manila.myshopify.com/apps/bundles/bundle/34280",
+    },
+}
 
 const FMM: FC = () => {
-
+  const week: number = moment().add(2, 'days').isoWeek()
+  const index: number = ((week + 1) % 4) + 1;
   return (
     <div>
       <Head>
@@ -12,12 +33,14 @@ const FMM: FC = () => {
           <div className="text-2xl px-8 mt-8 text-gray-700 font-bold">Food Market Shortcuts</div>
           <div 
             className="my-4 w-80 rounded-lg text-white bg-green-500 hover:bg-green-600 active:bg-green-800 p-4 mx-8 font-bold select-none cursor-pointer shadow" 
-            onClick={() => {navigator.clipboard.writeText("hello world!")}}>
-              1. Click to copy delivery message
+            onClick={() => {navigator.clipboard.writeText("Pls deliver food before 12nn, pls leave food at lobby")}}>
+              1. Click to Copy Delivery Message
           </div>
-          <div className="my-4 w-80 rounded-lg text-white bg-blue-400 hover:bg-blue-500 active:bg-blue-700 p-4 mx-8 font-bold select-none cursor-pointer shadow">
-              2. Go to order page
-          </div>
+          <a href={LINKS[index].url} target="_blank" rel="noreferrer">
+            <div className="my-4 w-80 rounded-lg text-white bg-blue-400 hover:bg-blue-500 active:bg-blue-700 p-4 mx-8 font-bold select-none cursor-pointer shadow">
+                2. Go to Page ({LINKS[index].name})
+            </div>
+          </a>
       </div>
     </div>
   )
